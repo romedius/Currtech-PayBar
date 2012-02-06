25a8fb6fb5c5b087b4404c3f20dc9932817199f9
Romedius Weiss <me@romediusweiss.com>
2012-02-06 14:28:27 +0100

    Some bugs fixed

---
+++ project/paybar-clearingHouse/clearinghouse-ejb/src/main/java/paybar/data/PaybarResourceException.java
@@ -0,0 +1,19 @@
+package paybar.data;
+
+
+public class PaybarResourceException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PaybarResourceException() {
+		super();
+	}
+
+	public PaybarResourceException(String msg) {
+		super(msg);
+	}
+
+}
